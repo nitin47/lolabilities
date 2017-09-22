9b9e55f7baf22ec7ed757c1450ce59ba590a2683
Nitin <nitin236@gmail.com>
2017-09-22 18:13:26 +0530

    Fixing mysql url.

--- src/main/java/com/eyequeue/lolabilities/util/Constants.java
+++ src/main/java/com/eyequeue/lolabilities/util/Constants.java
@@ -6,8 +6,8 @@ public class Constants {
 	public static final String LOCAL_MYSQL_USER = "root";
 	public static final String LOCAL_MYSQL_PASSWORD = "root";
 	
-	public static final String OPENSHIFT_MYSQL_URL = "jdbc:mysql://mysql:3306/lolabilitiesdb?autoReconnect=true&useSSL=false";
-	public static final String OPENSHIFT_MYSQL_USER = "root";
-	public static final String OPENSHIFT_MYSQL_PASSWORD = "MyDB#4790";
+	public static final String OPENSHIFT_MYSQL_URL = "jdbc:mysql://mysql-1-xr8vt:3306/lolabilitiesdb?autoReconnect=true&useSSL=false";
+	public static final String OPENSHIFT_MYSQL_USER = "admin";
+	public static final String OPENSHIFT_MYSQL_PASSWORD = "OreNoDB";
 
 }
--- src/main/java/com/eyequeue/lolabilities/util/Variables.java
+++ src/main/java/com/eyequeue/lolabilities/util/Variables.java
@@ -3,7 +3,7 @@ package com.eyequeue.lolabilities.util;
 public class Variables {
 	
 	public static final String MYSQL_URL = Constants.OPENSHIFT_MYSQL_URL;
-	public static final String MYSQL_USER = Constants.OPENSHIFT_MYSQL_URL;
+	public static final String MYSQL_USER = Constants.OPENSHIFT_MYSQL_USER;
 	public static final String MYSQL_PASSWORD = Constants.OPENSHIFT_MYSQL_PASSWORD;
 
 }
